a0f0bab034411d7540388ff2419793608aebc71b
irocho <irocho.profe@gmail.com>
2016-11-26 22:35:41 +0100

    modifiquei algo?

--- escripito.sh
+++ escripito.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 find latex/cachos/ -iname "*.tex" -type f -exec sh -c 'pandoc "${0}" -o "${0%.tex}.markdown"' {} \;
 mv ./latex/cachos/*.md ./_includes/
-
+echo ....
 git commit -a
 git push
 got status
