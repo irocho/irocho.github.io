46d22d9759a98a1c94c5475e91127e5c12b53af5
irocho <irocho.profe@gmail.com>
2016-11-26 22:33:11 +0100

    Algo falla

--- escripito.sh
+++ escripito.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 find latex/cachos/ -iname "*.tex" -type f -exec sh -c 'pandoc "${0}" -o "${0%.tex}.markdown"' {} \;
 mv ./latex/cachos/*.md ./_includes/
-echo "Mensaxe para github"
-read -r mensaxe
-git add *
-git commit -m '$mensaxe'
+
+git commit -a
 git push
+got status
+git log | head -n 5
