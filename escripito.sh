7edad12ab936be02562896df5e4626b45275165e
irocho <irocho.profe@gmail.com>
2016-11-26 22:38:19 +0100

    separado add de commit

--- escripito.sh
+++ escripito.sh
@@ -2,7 +2,8 @@
 find latex/cachos/ -iname "*.tex" -type f -exec sh -c 'pandoc "${0}" -o "${0%.tex}.markdown"' {} \;
 mv ./latex/cachos/*.md ./_includes/
 echo ....
-git commit -a
+git add *
+git commit
 echo ....
 git push
 echo ....
