c90fbc8b817627cc3639392f8b540e6d3bbb7dd9
irocho <irocho.profe@gmail.com>
2016-11-26 22:22:09 +0100

    $mensaxe

--- escripito.sh
+++ escripito.sh
@@ -2,7 +2,7 @@
 find latex/cachos/ -iname "*.tex" -type f -exec sh -c 'pandoc "${0}" -o "${0%.tex}.markdown"' {} \;
 mv ./latex/cachos/*.md ./_includes/
 echo "Mensaxe para github"
-read mensaxe
+read -r mensaxe
 git add *
-git commit -m $mensaxe
+git commit -m '$mensaxe'
 git push
